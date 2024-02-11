a9e01ab9f1a891221e19a90e807722f6d5cf94e1
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-02-11 17:06:26 +0100

    Import first 20 products from prepare api

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,58 +1,12 @@
 import { BaseHeading } from "@/ui/atoms/BaseHeading";
 import { ProductList } from "@/ui/organisms/Catalog/ProductList";
-import type { CatalogProduct } from "@/types/Catalog/Product";
+import { getProducts } from "@/api/products";
 
-const products: CatalogProduct[] = [
-	{
-		id: "1",
-		name: "Red sneakers #1",
-		category: "Shoes",
-		slug: "red-sneakers-1",
-		image: {
-			src: "/shoes1.jpeg",
-			alt: "Red sneakers with white soles and white laces. There are three shoes in the photo",
-		},
-		price: 1999,
-	},
-	{
-		id: "2",
-		name: "Red sneakers #2",
-		category: "Shoes",
-		slug: "red-sneakers-2",
-		image: {
-			src: "/shoes2.jpeg",
-			alt: "Red sneakers with white soles and red laces",
-		},
-		price: 2999,
-	},
-	{
-		id: "3",
-		name: "Red sneakers #3",
-		category: "Shoes",
-		slug: "red-sneakers-3",
-		image: {
-			src: "/shoes3.jpeg",
-			alt: "Red sneakers with a white sole, white laces and many white stripes",
-		},
-		price: 4999,
-	},
-	{
-		id: "4",
-		name: "Red sneakers #4",
-		category: "Shoes",
-		slug: "red-sneakers-4",
-		image: {
-			src: "/shoes4.jpeg",
-			alt: "Red sneakers with a white sole, white laces and many white stripes",
-		},
-		price: 5999,
-	},
-];
-
-export default function ProductsPage() {
+export default async function ProductsPage() {
+	const products = await getProducts();
 	return (
 		<section className="py-4">
-			<BaseHeading text="Our red shoes" />
+			<BaseHeading text="Our 20 products" />
 			<ProductList products={products} />
 		</section>
 	);
