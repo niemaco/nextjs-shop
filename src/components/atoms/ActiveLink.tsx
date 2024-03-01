5cfc25bed9b33aae06cdcac5fb617149f2159e53
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-01 08:19:54 +0100

    Statically Typed Links
    https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links

--- src/components/atoms/ActiveLink.tsx
+++ src/components/atoms/ActiveLink.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import React, { type ReactNode } from "react";
+import { type Route } from "next";
 
-export const ActiveLink = ({
+export const ActiveLink = <T extends string>({
 	href,
 	className,
 	activeClassName,
 	children,
 }: {
-	href: string;
+	href: Route<T> | URL;
 	className: string;
 	activeClassName: string;
 	children: ReactNode;
