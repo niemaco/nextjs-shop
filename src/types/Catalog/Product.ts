d54f3d4b833d095f456564d1418e0182c6f55cc0
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-02 16:14:46 +0100

    Add catalog product type

--- src/types/Catalog/Product.ts
+++ src/types/Catalog/Product.ts
@@ -15,7 +15,7 @@ export type CatalogProductDetails = {
 		name: string;
 		category: string;
 		price: number;
-		slug: string;
+		id: string;
 	};
 };
 
@@ -24,4 +24,20 @@ export type CatalogProductImage = {
 		src: string;
 		alt: string;
 	};
+};
+
+export type CatalogProductResponse = {
+	id: string;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	rating: Rating;
+	image: string;
+	longDescription: string;
+};
+
+type Rating = {
+	rate: number;
+	count: number;
 };
