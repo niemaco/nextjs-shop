b3dac09ae9702abf5fe275c11358c4920b0d4bca
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-02 16:13:26 +0100

    Add products list

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,18 +1,16 @@
-import { Suspense } from "react";
 import { BaseHeading } from "@/ui/atoms/BaseHeading";
 import { Pagination } from "@/ui/molecules/Catalog/Pagination";
 import { ProductList } from "@/ui/organisms/Catalog/ProductList";
 import { getProducts } from "@/api/products";
 
-export default async function ProductsPage() {
-	const products = await getProducts();
+export default async function ProductsPageById({ params }: { params: { id?: string } }) {
+	const pageId = parseInt(params?.id || "1");
+	const products = await getProducts(pageId);
 	return (
 		<section className="grid grid-cols-1 gap-4 py-4">
 			<BaseHeading text="Our 20 products" />
 			<Pagination />
-			<Suspense>
-				<ProductList products={products} />
-			</Suspense>
+			<ProductList products={products} />
 			<Pagination />
 		</section>
 	);
