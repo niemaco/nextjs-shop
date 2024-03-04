63b8b275413ccd1b65d68c563e3458203d635590
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-04 15:22:11 +0100

    Add first icons

--- src/components/molecules/MainNav.tsx
+++ src/components/molecules/MainNav.tsx
@@ -1,36 +1,58 @@
+import { ShoppingCart } from "lucide-react";
 import { ActiveLink, type ActiveLinkProps } from "@/ui/atoms/ActiveLink";
+import { BaseIcon } from "@/ui/atoms/BaseIcon";
 
 const className: string =
-	"p-4 first:pl-0 last:pr-0 text-grey-400 hover:text-blue-400 focus-visible:text-blue-400";
+	"text-gray-600 group-hover:text-blue-400 group-focus-visible:text-blue-400";
 
-const navLinks: ActiveLinkProps<string>[] = [
+type NavLink = ActiveLinkProps<string> & {
+	icon?: string;
+};
+
+const navLinks: NavLink[] = [
 	{
 		children: "Home",
 		href: "/",
 		exact: true,
+		icon: "home",
 	},
 	{
 		children: "All",
 		href: "/products",
+		icon: "layout-list",
 	},
 	{
 		children: "Blog",
 		href: "/blog",
+		icon: "notebook",
 	},
 ];
 
 export const MainNav = () => {
 	return (
-		<nav>
-			<ul className="list-none">
-				<li>
-					{navLinks.map(({ href, children, exact }, index) => (
+		<nav className="flex items-center justify-between">
+			<ul className="flex list-none flex-wrap">
+				{navLinks.map(({ href, children, exact, icon }, index) => (
+					<li key={index} className="group flex items-center gap-x-2 p-4 first:pl-0 last:pr-0 ">
+						{icon && (
+							<BaseIcon
+								name={icon}
+								size={20}
+								className="group-hover:text-blue-400 group-focus-visible:text-blue-400"
+							/>
+						)}
 						<ActiveLink key={index} href={href} className={className} exact={exact}>
 							{children}
 						</ActiveLink>
-					))}
-				</li>
+					</li>
+				))}
 			</ul>
+
+			<div className="flex text-gray-600">
+				<ShoppingCart className="mr-2" />
+				<span>0</span>
+				<span className="sr-only">Shopping Cart</span>
+			</div>
 		</nav>
 	);
 };
