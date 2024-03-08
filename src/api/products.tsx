9cd6e8225c7513803c2c7e91f63ac88db2bb3549
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-08 08:42:09 +0100

    Clean up

--- src/api/products.tsx
+++ src/api/products.tsx
@@ -2,10 +2,6 @@ import { ProductsGetDocument } from "@/gql/graphql";
 import { executeGraphql } from "@/api/graphql";
 
 export const getProducts = async (page: number = 1, limit = 12) => {
-	if (!process.env.GRAPHQL_URL) {
-		throw TypeError("GRAPHQL_URL is not defined");
-	}
-
 	const offset = limit * (page - 1);
 	const graphqlResponse = await executeGraphql(ProductsGetDocument, {
 		offset,
