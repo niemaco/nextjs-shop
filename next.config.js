8b68e920edd48ab95fe3a16b9e59af05c19c707c
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-08 10:40:11 +0100

    Add redirects

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
 				destination: "/products/1",
 				permanent: true,
 			},
+			{
+				source: "/categories/:slug",
+				destination: "/categories/:slug/1",
+				permanent: true,
+			},
 		];
 	},
 };
