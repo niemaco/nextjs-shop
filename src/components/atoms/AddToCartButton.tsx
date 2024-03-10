a62e87d1fe09dd458e13838265f556299b9798e5
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-10 01:00:58 +0100

    Style disabled button

--- src/components/atoms/AddToCartButton.tsx
+++ src/components/atoms/AddToCartButton.tsx
@@ -9,7 +9,7 @@ export function AddToCartButton() {
 		<button
 			type="submit"
 			disabled={status.pending}
-			className="mb-4 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+			className="mb-4 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 disabled:cursor-wait disabled:bg-gray-200 disabled:text-gray-200 disabled:text-gray-500"
 		>
 			Add to cart
 		</button>
