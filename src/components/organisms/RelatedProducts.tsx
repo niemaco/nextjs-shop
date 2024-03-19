04ddb7ea78c357ec51aa25d722348c5eb9d1326f
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-19 13:14:26 +0100

    Fix data-testid

--- src/components/organisms/RelatedProducts.tsx
+++ src/components/organisms/RelatedProducts.tsx
@@ -9,7 +9,7 @@ export const RelatedProducts = ({ products }: RelatedProductsProps) => {
 	return (
 		<ul
 			className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4"
-			data-testid="related-products"
+			data-testid="products-list"
 		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
