ca97586b6f7e9f4545700d472a1c60539c215e1d
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-19 08:33:32 +0100

    Set href to hompage

--- src/app/payment/cancelled/page.tsx
+++ src/app/payment/cancelled/page.tsx
@@ -4,7 +4,7 @@ const PaymentCancelledPage = async () => {
 	return (
 		<>
 			<h1>Cart cancelled</h1>
-			<Link href="/public">Return to home page</Link>
+			<Link href={"/"}>Return to home page</Link>
 		</>
 	);
 };
