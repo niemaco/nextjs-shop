316ca6d01c6ba2a7e1711bab71acb03f09096892
Karol Osuchowski <karol.osuchowski@snow.dog>
2024-03-19 08:35:33 +0100

    Fix stripe api version

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -14,7 +14,7 @@ export default async function CheckoutPage() {
 	}
 
 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-		apiVersion: "2022-11-15",
+		apiVersion: "2023-10-16",
 		typescript: true,
 	});
 
